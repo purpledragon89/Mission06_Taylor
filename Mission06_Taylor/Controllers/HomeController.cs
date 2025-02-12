55126299a13db6084a41ff3cb55000c23ff133ea
purpledragon89 <rileywtaylor@gmail.com>
2025-02-12 10:40:02 -0700

    new file tst

--- Mission06_Taylor/Controllers/HomeController.cs
+++ Mission06_Taylor/Controllers/HomeController.cs
@@ -1,31 +1,61 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Mission06_Taylor.Models;
 
-namespace Mission06_Taylor.Controllers;
-
-public class HomeController : Controller
+namespace Mission06_Taylor.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly movieContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        //  Single constructor for both logger and database context
+        public HomeController(ILogger<HomeController> logger, movieContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Bio()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Collection()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Collection(Movie model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(model);
+                _context.SaveChanges(); // Save to database
+                
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Index");
+        }
     }
 }
--- Mission06_Taylor/Models/movieContext.cs
+++ Mission06_Taylor/Models/movieContext.cs
@@ -1,6 +1,15 @@
-namespace Mission06_Taylor.Models;
+using System.Net.Mime;
+using Microsoft.EntityFrameworkCore;
 
-public class movieContext
+namespace Mission06_Taylor.Models
 {
-    
+    public class movieContext : DbContext
+    {
+        public movieContext(DbContextOptions<movieContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+
 }
